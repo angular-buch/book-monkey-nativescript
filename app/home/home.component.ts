1c455a759196ec3a623c7bfb783fd2746d042808
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-03-28 22:05:19 +0200

    update to `as`-syntax

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   template: `
     <StackLayout>
       <Label class="heading" text="Home"></Label>
-      <Label text="Das ist der BookMonkey"></Label>
+      <Label text="Das ist der BookMonkey mit Angular 4"></Label>
       <Button nsRouterLink="../books" text="Buchliste ansehen" class="ui red button"></Button>
     </StackLayout>
   `
